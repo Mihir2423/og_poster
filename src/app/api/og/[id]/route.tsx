92f5e4ca661638f64c118ffe432a438c9611f7d5
Mihir2423 <mihirraj444@gmail.com>
2024-08-06 01:22:14 +0530

    Update route.tsx

--- src/app/api/og/[id]/route.tsx
+++ src/app/api/og/[id]/route.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/og";
 
 export async function GET(req: Request, context: any) {
@@ -19,48 +19,39 @@ export async function GET(req: Request, context: any) {
 
     return new ImageResponse(
       (
-        <div
-          style={{
-            fontSize: 40,
-            color: "black",
-            background: "white",
-            width: "100%",
-            height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <h1
+        <div tw="flex flex-col gap-2 items-center w-full h-full justify-center p-8 bg-white text-black text-base text-black">
+          <div
+            tw="shadow-gray-200 shadow-md my-6 p-4 rounded-xl w-[380px] flex flex-col relative"
             style={{
-              fontSize: 32,
+              background:
+                "linear-gradient(180deg, rgba(252,244,244,1) 0%, rgba(234,234,247,1) 42%, rgba(0,0,0,0.6031600140056023) 80%)",
             }}
           >
-            {post.title || "No Title Provided"}
-          </h1>
-          <h1
-            style={{
-              fontSize: 32,
-              transform: "translateY(-60px)",
-            }}
-          >
-            {post.description?.length > 30 ? post.description.substring(0, 30) + "..." : post.description  || "No Description Provided"}
-          </h1>
-          {post?.image && (
-            <img
-              src={post.image}
-              alt="image"
-              onError={(e) => {
-                e.currentTarget.src = "/error-image.png";
-              }}
-              width={360}
-              height={360}
-              style={{ borderRadius: 20, transform: "translateY(-60px)" }}
-            />
-          )}
+            {post?.image && (
+              <div tw="flex justify-center py-2 w-full">
+                <img
+                  src={post.image}
+                  alt="image"
+                  onError={(e) => {
+                    e.currentTarget.src = "/error-image.png";
+                  }}
+                  width={300}
+                  height={300}
+                  tw="rounded-md w-full max-w-[360px] max-h-[300px] object-center"
+                />
+              </div>
+            )}
+            <div tw="flex flex-col border-gray-400 mt-3 pt-3 border-t">
+              <p tw="text-[16px] p-0 m-0 text-black">
+                {post.title || "No Title Provided"}
+              </p>
+              <p tw="text-[14px] text-gray-900 p-0 m-0">
+                {post.description?.length > 40
+                  ? post.description.substring(0, 40) + "..."
+                  : post.description || "No Description Provided"}
+              </p>
+            </div>
+          </div>
         </div>
       ),
       {
